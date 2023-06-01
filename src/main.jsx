8ca868a99e57d2227b991b4cf365a13943b6aaee
Necelentano <necelentano@gmail.com>
2023-06-01 11:50:58 +0300

    Return BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 import Header from "./components/Header.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
